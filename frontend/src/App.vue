3a9eaafdc98182472e782387a04c393b421f10f7
Joel Pita Vidal <a25joelpv@teis.intranet>
2026-03-04 11:14:49 +0100

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex flex-column min-vh-100 justify-content-center">
+    <div class ="w-75 w-lg-80 mx-auto">
+    <Navbar />
+    <main class="flex-grow-1 w-75 mx-auto w-lg-80">
+      <router-view />
+    </main>
+    <FooTer />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "./components/NavBar.vue";
+import FooTer from "./components/FooTer.vue"
+</script>
+
+<style scooped>
+@media (max-width:780px) {
+  boddy {
+    background: ligthgray;
+  }
+}
+</style>
---
+++ frontend/src/components/TareaS.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="text-center-mb4"></h1>
+        <p class="lead text-center">
+        Esta es la página de información sobre tareas en nuestro sistema de gestión de tareas
+        </p>
+    </div>
+</template>
+<script setup></script>
+<style scooped></style>
